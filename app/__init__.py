e9781f12ea881c7d77cfe30c59a2f1a0c60b9c79
rui zhang <ruizhang@ruis-MacBook-Pro.local>
2017-01-17 15:48:57 -0800

    user login and DB hook

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-import os
 from flask_login import LoginManager
 from flask_openid import OpenID
 from config import basedir
@@ -9,8 +8,9 @@ from config import basedir
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-from app import views,model
 lm = LoginManager()
 lm.init_app(app)
+lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from app import views, model
