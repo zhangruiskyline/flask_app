7724353f8e77e00bdeb130a45b3507f971eab0ba
rui zhang <ruizhang@ruis-MacBook-Pro.local>
2017-01-19 00:19:33 -0800

    sign in

--- app/model.py
+++ app/model.py
@@ -2,6 +2,7 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
