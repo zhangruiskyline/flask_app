ca1b5cb748fe00740ac0f53b2b622568d8c5fa32
rui zhang <zhangruiskyline@gmail.com>
2017-01-19 17:31:51 -0800

    user edit page and db support

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,11 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm, Form
+from wtforms import StringField, BooleanField,TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
     user_name = StringField('user_name', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+class EditForm(Form):
+    nickname = StringField('nickname', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required, LoginManager
 from app import app, db, lm
-from .forms import LoginForm
+from .forms import LoginForm, EditForm
 from .model import User
+from datetime import datetime
 
 
 @lm.user_loader
@@ -12,6 +13,10 @@ def user_loader(id):
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 
 @app.route('/')
@@ -75,3 +80,19 @@ def user(nickname):
     return render_template('user.html',
                            user=user,
                            posts=posts)
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
