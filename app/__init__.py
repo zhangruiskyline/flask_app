8526725c103c765eec58b2c6b431f9c76fd2a759
rui zhang <zhangruiskyline@gmail.com>
2017-01-20 14:31:53 -0800

    follower/following update

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,7 @@ if not app.debug:
 if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('../tmp/status.log', 'a',
+    file_handler = RotatingFileHandler('status.log', 'a',
                                        1 * 1024 * 1024, 10)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter(
