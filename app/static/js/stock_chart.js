16e5c20c3a4eefeb8e93dba7ea72fc329d37a833
rui zhang <zhangruiskyline@gmail.com>
2017-02-02 23:47:22 -0800

    highcharts working

---
+++ app/static/js/stock_chart.js
@@ -0,0 +1,32 @@
+/**
+ * Created by ruizhang on 1/27/17.
+ */
+// START CONFIGURATION
+/**
+ * Created by ruizhang on 2/2/17.
+ */
+$(document).ready(function () {
+    $('#chart-container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Stock chart'
+        },
+        xAxis: {
+            categories: ['Market Cap', 'Margin', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Normalized'
+            }
+        },
+        series: [{
+            name: 'AAPL',
+            data: [1, 0, 4]
+        }, {
+            name: 'QCOM',
+            data: [5, 7, 3]
+        }]
+    });
+});
